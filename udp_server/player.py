f71f89baf52555a6cdb066960b59dbd88aea44fa
Hector Soria <hector.soria@hotmail.com>
2019-12-25 16:32:38 +0100

    completing udp protocol

---
+++ udp_server/player.py
@@ -0,0 +1,123 @@
+from twisted.internet.defer import inlineCallbacks as inline_callbacks
+from twisted.internet.threads import deferToThread as defer_to_thread
+
+from apps.cache import (is_dirty,
+                        remove_logged_player, set_clean,
+                        update_player_game_data_cache)
+from apps.games.models import Game
+from apps.players.serializers import (
+    PlayerTransformSerializer, PlayerMovedSerializer, PlayerJoinedGameSerializer, GamePlayersSerializer,
+    PlayerLeftGameSerializer, GameJoinedSerializer
+)
+
+from settings import RESPONSE_PLAYER_LEFT, RESPONSE_GAME_PLAYERS, RESPONSE_PLAYER_JOINED, RESPONSE_JOINED_GAME
+
+
+class Player:
+    def __init__(self, protocol, player_state, address):
+        self.address = address
+        self.protocol = protocol
+        self.player_state = player_state
+
+        self.pk = player_state.pk
+
+        self.actions = {
+            # "positionUpdate": self.position_update,
+            "move": self.move,
+        }
+
+    @inline_callbacks
+    def join_game(self):
+        game, players = yield defer_to_thread(self.player_state.add_to_default_game)
+        yield self.protocol.add_to_group(game.key, self.address)
+        data = {
+            "players": GamePlayersSerializer(players, many=True).data
+        }
+        self.protocol.send(self.address, RESPONSE_JOINED_GAME, data=GameJoinedSerializer(game).data)
+        self.protocol.send(self.address, RESPONSE_GAME_PLAYERS, data=data)
+        self.protocol.queue_to_broadcast(
+            RESPONSE_PLAYER_JOINED,
+            exclude_sender=True,
+            data=PlayerJoinedGameSerializer(self.player_state).data,
+            sender=self.address,
+            group_name=self.player_state.game.key
+        )
+
+    @inline_callbacks
+    def execute_action(self, action, data):
+        yield self.refresh_state()
+        yield self.actions[action](data)
+
+    @inline_callbacks
+    def refresh_state(self):
+        if is_dirty(self.player_state.key):
+            set_clean(self.player_state.key)
+            yield defer_to_thread(self.player_state.refresh_from_db)
+        else:
+            try:
+                self.player_state.game
+            except Game.DoesNotExist:
+                yield defer_to_thread(self.player_state.refresh_from_db)
+
+    @inline_callbacks
+    def _disconnect_from_game(self):
+        if self.player_state.game:
+            yield self.protocol.unregister_from_group(self.player_state.game.key, self.address)
+            self.protocol.queue_to_broadcast(
+                RESPONSE_PLAYER_LEFT,
+                data=PlayerLeftGameSerializer(self.player_state).data,
+                sender=self.address,
+                group_name=self.player_state.game.key
+            )
+
+            yield defer_to_thread(self.player_state.quit_game)
+
+    @inline_callbacks
+    def on_disconnect(self):
+        self.refresh_state()
+        yield self._disconnect_from_game()
+
+        yield defer_to_thread(remove_logged_player, self.player_state.key)
+
+    @inline_callbacks
+    def position_update(self, message):
+        if self.player_state.game:
+            serializer = PlayerTransformSerializer({'key': self.player_state.key, **message})
+            yield defer_to_thread(
+                update_player_game_data_cache,
+                self.player_state.key,
+                self.player_state.game.key,
+                serializer.data
+            )
+
+    @inline_callbacks
+    def quit_game(self, _):
+        if self.player_state.game:
+            yield self.protocol.unregister_from_group(self.player_state.game.key, self.address)
+            self.protocol.queue_to_broadcast(
+                RESPONSE_PLAYER_LEFT,
+                data={
+                    "player_id": self.player_state.key
+                },
+                sender=self.address,
+                group_name=self.player_state.game.key
+            )
+            yield defer_to_thread(self.player_state.quit_game)
+
+    @inline_callbacks
+    def move(self, message):
+        if self.player_state.game:
+            serializer = PlayerMovedSerializer(message)
+            '''
+            if not serializer.is_valid():
+                self.protocol.send_error(serializer.error_messages(), self.address)
+                return
+            '''
+            data = dict(serializer.data)
+            data['key'] = self.player_state.key
+            yield defer_to_thread(
+                update_player_game_data_cache,
+                self.player_state.game.key,
+                self.player_state.key,
+                data
+            )
--- udp_server/protocol.py
+++ udp_server/protocol.py
@@ -2,21 +2,159 @@
 
 # Copyright (c) Twisted Matrix Laboratories.
 # See LICENSE for details.
+import json
 from colorama import Fore
-
+from twisted.internet.defer import inlineCallbacks as inline_callbacks
 from twisted.internet.protocol import DatagramProtocol
-from twisted.internet import reactor
+from twisted.internet.threads import deferToThread as defer_to_thread
 from twisted.logger import Logger
+
+from twisted.internet.defer import inlineCallbacks as inline_callbacks
+from twisted.internet.threads import deferToThread as defer_to_thread
+from apps.cache import (add_logged_player, get_logged_players,
+                        set_player_to_client, add_to_group, remove_from_group,
+                        get_clients_from_group, add_message_to_broadcast_queue)
+
+from apps.players.models import Player as PlayerState
+from apps.players.serializers import (AuthSerializer, SendAuthSerializer)
+from udp_server.player import Player
+
+from settings import RESPONSE_PLAYER_ALREADY_LOGGED, RESPONSE_AUTH_FAILURE, RESPONSE_AUTH_PLAYER, RESPONSE_PONG
+
 
 logger = Logger()
 
 
 # Here's a UDP version of the simplest possible protocol
 class SocketProtocol(DatagramProtocol):
+    register_action = 'auth'
+    ignore_actions = ["move", "PLAYERS_TRANSFORM"]
+    connections = {}
+
     def __init__(self, service):
         self.service = service
         DatagramProtocol.__init__(self)
+        actions = {
+            "auth": self.auth,
+            "ping": self.ping
+        }
 
-    def datagramReceived(self, datagram, address):
-        logger.info(Fore.RED + '\u21E6' + Fore.RESET + ' Receivning ::: {}'.format(datagram))
+    def send_error(self, msg, address):
+        datagram = json.dumps({'action': 'error', 'data': msg}).encode()
         self.transport.write(datagram, address)
+
+    def send(self, address, action, data={}):
+        if action not in self.ignore_actions:
+            if action == 'error':
+                logger.info(Fore.GREEN + '\u2192' + Fore.RESET + ' Sending ::: {} - Data {}'.format(action, data))
+            else:
+                logger.info(Fore.GREEN + '\u2192' + Fore.RESET + ' Sending ::: {}'.format(action))
+
+        if data is None:
+            data = {}
+        response = {'action': action, 'data': json.dumps(data)}
+        datagram = json.dumps(response).encode('utf8')
+        self.transport.write(datagram, address)
+
+
+    @inline_callbacks
+    def queue_to_broadcast(self, action, data=None, exclude_sender=False, sender=None, group_name=None):
+        if not group_name:
+            raise Exception("We need a group name to broadcast!")
+        for _sender in get_clients_from_group(group_name):
+            if exclude_sender and sender == _sender:
+                continue
+            yield defer_to_thread(add_message_to_broadcast_queue, _sender, action, data)
+
+    @inline_callbacks
+    def add_to_group(self, group_name, sender):
+        yield defer_to_thread(add_to_group, group_name, sender)
+
+    @inline_callbacks
+    def unregister_from_group(self, group_name, sender):
+        yield defer_to_thread(remove_from_group, group_name, sender)
+
+
+    @inline_callbacks
+    def datagramReceived(self, datagram, address):
+        msg = json.loads(datagram.decode("utf-8"))
+
+        try:
+            msg = json.loads(datagram.decode('utf8'))
+        except ValueError:
+            self.send_error({'message': 'Invalid JSON.'})
+            return
+
+        if 'action' not in msg:
+            self.send_error({'message': 'Action needed.'})
+            return
+        try:
+            data = json.loads(msg.get('data', {}))
+        except ValueError:
+            self.send_error({'message': 'Invalid JSON.'})
+            return
+
+        action = msg['action']
+        sent = False
+
+        if action not in self.ignore_actions:
+            logger.info(Fore.RED + '\u21E6' + Fore.RESET + ' Receivning ::: {}'.format(action))
+
+        if action in self.actions:
+            sent = True
+            yield self.actions[action](data, address)
+
+        if self.connections.get(address) and action in self.connections[address].actions:
+            sent = True
+            yield self.player.execute_action(action, data)
+
+        if action in self.service.actions:
+            sent = True
+            yield self.service.actions[action](self, data, address)
+
+        if not sent:
+            self.send_error("Action {} not allowed".format(action))
+
+    @inline_callbacks
+    def auth(self, message, address):
+        if self.connections.get(address) is not None:
+            return
+
+        serializer = AuthSerializer(data=message)
+        if not serializer.is_valid():
+            self.send_error(serializer.errors)
+            return
+
+        player_state, error = yield defer_to_thread(
+            PlayerState.objects.auth,
+            serializer.data['name']
+        )
+        if not player_state:
+            self.send(RESPONSE_AUTH_FAILURE, data=error)
+            return
+
+        logged_players = yield defer_to_thread(get_logged_players)
+        if player_state.key in logged_players:
+            self.send(
+                RESPONSE_PLAYER_ALREADY_LOGGED,
+                data=serializer.data['name']
+            )
+
+        yield defer_to_thread(add_logged_player, player_state.key)
+        yield defer_to_thread(set_player_to_client, player_state.key, self.key)
+
+        self.connections[address] = Player(self, player_state, address)
+
+        serializer = SendAuthSerializer(player_state)
+
+        self.send(
+            address,
+            RESPONSE_AUTH_PLAYER,
+            data=serializer.data
+        )
+        yield self.player.join_game()
+
+    @inline_callbacks
+    def ping(self, _, _):
+        yield
+        self.send(RESPONSE_PONG, data={"message": "pong"})
